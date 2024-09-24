5522bad9ee26ade8b93c6f635587811d6d8243bf
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-09-24 18:18:21 -0500

    feat: can navigate back

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <header>
+      <nav class="container mx-auto p-4">
+        <ul>
+          <li><a href="/" class="hover:underline">Courses</a></li>
+        </ul>
+      </nav>
+    </header>
     <router-view />
   </div>
 </template>
